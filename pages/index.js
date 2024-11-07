a5182351ee61722849cc0e10db5b047480543e97
Harif-Hadi <hadi4harif@gmail.com>
2024-11-07 06:25:45 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hadikus Enterprise</title>
+        <meta name="description" content="Online shop" />
+      </Head>
+      <h1>The homepage</h1>
+    </div>
+  );
+}
